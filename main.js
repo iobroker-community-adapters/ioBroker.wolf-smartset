e9644104b7f0e78a0ca88470f0d0f51cf24261d5
MeisterTR <rensen26@gmail.com>
2021-02-11 16:03:43 +0100

    add

--- main.js
+++ main.js
@@ -7,9 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const WSS = require(__dirname + '/lib/wolfsmartset');
 
 class WolfSmartset extends utils.Adapter {
 
