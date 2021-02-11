0298496e4a19b33e3322e36f6002cdf24c7829b2
MeisterTR <rensen26@gmail.com>
2021-02-11 06:25:59 +0100

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'wolf-smartset adapter settings': {
+		'en': 'Adapter settings for wolf-smartset',
+		'de': 'Adaptereinstellungen für wolf-smartset',
+		'ru': 'Настройки адаптера для wolf-smartset',
+		'pt': 'Configurações do adaptador para wolf-smartset',
+		'nl': 'Adapterinstellingen voor wolf-smartset',
+		'fr': "Paramètres d'adaptateur pour wolf-smartset",
+		'it': "Impostazioni dell'adattatore per wolf-smartset",
+		'es': 'Ajustes del adaptador para wolf-smartset',
+		'pl': 'Ustawienia adaptera dla wolf-smartset',
+		'zh-cn': 'wolf-smartset的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
